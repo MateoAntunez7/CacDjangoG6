9bf3e58e8dca2b4180f229930c514b0050613ead
Melina Yangüez <67840710+melina8444@users.noreply.github.com>
2023-09-17 18:22:41 -0300

    acomode los templates, y cree tratamientos_listados con un filter

--- web_turnos/turnos/urls.py
+++ web_turnos/turnos/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('contacto/', views.contacto, name='contacto'),
     path('nosotros/', views.nosotros, name='nosotros'),
-    path('tratamientos/', views.tratamientos, name='tratamiemtos'),
+    path('tratamientos/', views.tratamientos, name='tratamientos'),
+    path('tratamientos/listado', views.tratamientos_listado, name='tratamientos_listado'),
     path('experiencias/', views.experiencias, name='experiencias')
 ]
